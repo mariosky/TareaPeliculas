9f29a10cac3e0d55b88f61c24c4dd63c5104ebab
Mario Garcia Valdez <mariosky@gmail.com>
2021-03-10 12:45:33 -0800

    Update Program.cs

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -9,69 +9,25 @@ namespace Pelicula
         //Campos
         private List<Actor> actores = new List<Actor>();
 
-        private string titulo;
-        private Int16 año;
         //Constructores
-        public Pelicula()
-        { }
-
-        public Pelicula(string t, Int16 a)
-        {
-            titulo = t;
-            año = a;
-        }
+     
         //Métodos
-        public void SetTitulo(string t)
-        {
-            titulo = t;
-        }
-        public void SetAño(Int16 a)
-        {
-            año = a;
-        }
-
-        public string GetTitulo() => titulo;
-
-        public Int16 GetAño() => año;
-
+     
         public void Imprime()
         {
-            Console.WriteLine($"{titulo} ({año})");
+     //       Console.WriteLine($"{titulo} ({año})");
 
         }
 
-        public void ImprimeActores()
-        {
-            foreach (var a in actores)
-                a.Imprime();
-        }
-
-        public void AgregaActor(Actor a)
-        {
-            actores.Add(a);
-        }
 
     }
 
     public class Actor
     {
-        //Atributos
-        private string nombre;
-        private int año;
-
         //Propiedades
 
         //Constructores
 
-        public Actor(string n, int a)
-        {
-            Nombre = n;
-            Año = a;
-
-        }
-
-        public string Nombre { get => nombre; set => nombre = value; }
-        public int Año { get => año; set => año = value; }
 
         //Métodos 
         public void Imprime()
@@ -89,14 +45,7 @@ namespace Pelicula
 
         static void Main(string[] args)
         {
-            Pelicula p = new Pelicula("La la land", 2016);
-            p.Imprime();
-            Pelicula p1 = new Pelicula("La La Land", 2016);
-            p1.AgregaActor(new Actor("Ryan Gosling", 1980));
-            p1.AgregaActor(new Actor("Emma Stone", 1988));
-
-            p1.ImprimeActores();
-
+            Console.WriteLine("Hello");
         }
     }
 }
